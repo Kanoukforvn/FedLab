6514a80603d243028c6de8ac9c878249bc77e73e
Kanoukforvn <charly.fonb@gmail.com>
2024-04-19 16:45:53 +0700

    fednoro model implementation

--- fednoro/fednoro_standalone.py
+++ fednoro/fednoro_standalone.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import torch
 
 sys.path.append("../")
 
@@ -9,8 +10,9 @@ sys.path.append(project_root)
 # configuration
 from munch import Munch
 from fedlab.models.mlp import MLP
+from fedlab.models.build_model import build_model
 
-model = MLP(784, 10)
+
 args = Munch
 
 args.total_client = 5
@@ -19,6 +21,15 @@ args.seed = 42
 args.preprocess = True
 args.cuda = False
 args.dataname = "cifar10"
+args.model = "Resnet18"
+args.pretrained = 1
+
+if args.dataname == "cifar10":
+    args.n_classes = 10
+
+args.device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+model = build_model(args)
 
 # We provide a example usage of patitioned CIFAR10 dataset
 # Download raw CIFAR10 dataset and partition them according to given configuration
@@ -30,14 +41,97 @@ fed_cifar10 = PartitionedCIFAR10(root="../datasets/cifar10/",
                          path="../datasets/cifar10/fedcifar10/",
                          dataname = args.dataname,
                          num_clients=args.total_client,
-                         partition="noniid-labeldir",
+                         partition="dirichlet",
                          dir_alpha=args.alpha,
                          seed=args.seed,
                          preprocess=args.preprocess,
                          download=True,
                          verbose=True,
-                         transform=transforms.Compose(
-                             [transforms.ToPILImage(), transforms.ToTensor()]))
+                         transform=transforms.ToTensor())
 
 dataset = fed_cifar10.get_dataset(0) # get the 0-th client's dataset
 dataloader = fed_cifar10.get_dataloader(0, batch_size=128) # get the 0-th client's dataset loader with batch size 128
+
+# client
+from fedlab.contrib.algorithm.basic_client import SGDSerialClientTrainer, SGDClientTrainer
+
+# local train configuration
+args.epochs = 5
+args.batch_size = 128
+args.lr = 0.1
+
+trainer = SGDSerialClientTrainer(model, args.total_client, cuda=args.cuda) # serial trainer
+# trainer = SGDClientTrainer(model, cuda=True) # single trainer
+
+trainer.setup_dataset(fed_cifar10)
+trainer.setup_optim(args.epochs, args.batch_size, args.lr)
+
+# server
+from fedlab.contrib.algorithm.basic_server import SyncServerHandler
+
+# global configuration
+args.com_round = 10
+args.sample_ratio = 0.1
+
+handler = SyncServerHandler(model=model, global_round=args.com_round, sample_ratio=args.sample_ratio, cuda=args.cuda)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from fedlab.utils.functional import evaluate
+from fedlab.core.standalone import StandalonePipeline
+
+from torch import nn
+from torch.utils.data import DataLoader
+import torchvision
+
+class EvalPipeline(StandalonePipeline):
+    def __init__(self, handler, trainer, test_loader):
+        super().__init__(handler, trainer)
+        self.test_loader = test_loader 
+        self.loss = []
+        self.acc = []
+        
+    def main(self):
+        t=0
+        while self.handler.if_stop is False:
+            # server side
+            sampled_clients = self.handler.sample_clients()
+            broadcast = self.handler.downlink_package
+            
+            # client side
+            self.trainer.local_process(broadcast, sampled_clients)
+            uploads = self.trainer.uplink_package
+
+            # server side
+            for pack in uploads:
+                self.handler.load(pack)
+
+            loss, acc = evaluate(self.handler.model, nn.CrossEntropyLoss(), self.test_loader)
+            print("Round {}, Loss {:.4f}, Test Accuracy {:.4f}".format(t, loss, acc))
+            t+=1
+            self.loss.append(loss)
+            self.acc.append(acc)
+    
+    def show(self):
+        plt.figure(figsize=(8,4.5))
+        ax = plt.subplot(1,2,1)
+        ax.plot(np.arange(len(self.loss)), self.loss)
+        ax.set_xlabel("Communication Round")
+        ax.set_ylabel("Loss")
+        
+        ax2 = plt.subplot(1,2,2)
+        ax2.plot(np.arange(len(self.acc)), self.acc)
+        ax2.set_xlabel("Communication Round")
+        ax2.set_ylabel("Accuarcy")
+        
+        
+test_data = torchvision.datasets.CIFAR10(root="../datasets/cifar10/",
+                                       train=False,
+                                       transform=transforms.ToTensor())
+test_loader = DataLoader(test_data, batch_size=1024)
+
+standalone_eval = EvalPipeline(handler=handler, trainer=trainer, test_loader=test_loader)
+standalone_eval.main()
+
+standalone_eval.show()
