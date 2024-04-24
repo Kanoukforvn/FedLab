acd1f49db91441f533bdcd031aee9008629c08c0
Kanoukforvn <charly.fonb@gmail.com>
2024-04-24 16:17:51 +0700

    debug losses

--- fedlab/utils/losses.py
+++ fedlab/utils/losses.py
@@ -13,8 +13,10 @@ class LogitAdjust(nn.Module):
         self.weight = weight
 
     def forward(self, x, target):
-        x_m = x + self.m_list
+        device = x.device
+        x_m = x + self.m_list.to(device=device)
         return F.cross_entropy(x_m, target, weight=self.weight)
+
 
 class LA_KD(nn.Module):
     def __init__(self, cls_num_list, tau=1):
@@ -25,7 +27,8 @@ class LA_KD(nn.Module):
         self.m_list = m_list.view(1, -1).to(device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
     def forward(self, x, target, soft_target, w_kd):
-        x_m = x + self.m_list
+        device = x.device
+        x_m = x + self.m_list.to(device=device)
         log_pred = torch.log_softmax(x_m, dim=-1)
         log_pred = torch.where(torch.isinf(log_pred), torch.full_like(log_pred, 0), log_pred)
 
