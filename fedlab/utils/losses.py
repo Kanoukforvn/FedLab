15647040e206f1fba5baa14f59e8d01fe297677a
Kanoukforvn <charly.fonb@gmail.com>
2024-04-24 15:58:40 +0700

    fixed loss

--- fedlab/utils/losses.py
+++ fedlab/utils/losses.py
@@ -6,24 +6,23 @@ import torch.nn.functional as F
 class LogitAdjust(nn.Module):
     def __init__(self, cls_num_list, tau=1, weight=None):
         super(LogitAdjust, self).__init__()
-        cls_num_list = torch.tensor(cls_num_list, dtype=torch.float64, device='cuda')
+        cls_num_list = torch.tensor(cls_num_list, dtype=torch.float64)
         cls_p_list = cls_num_list / cls_num_list.sum()
         m_list = tau * torch.log(cls_p_list)
-        self.m_list = m_list.view(1, -1)
+        self.m_list = m_list.view(1, -1).to(device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
         self.weight = weight
 
     def forward(self, x, target):
         x_m = x + self.m_list
         return F.cross_entropy(x_m, target, weight=self.weight)
 
-
 class LA_KD(nn.Module):
     def __init__(self, cls_num_list, tau=1):
         super(LA_KD, self).__init__()
-        cls_num_list = torch.tensor(cls_num_list, dtype=torch.float64, device='cuda')
+        cls_num_list = torch.tensor(cls_num_list, dtype=torch.float64)
         cls_p_list = cls_num_list / cls_num_list.sum()
         m_list = tau * torch.log(cls_p_list)
-        self.m_list = m_list.view(1, -1)
+        self.m_list = m_list.view(1, -1).to(device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
     def forward(self, x, target, soft_target, w_kd):
         x_m = x + self.m_list
@@ -33,4 +32,3 @@ class LA_KD(nn.Module):
         kl = F.kl_div(log_pred, soft_target, reduction='batchmean')
 
         return w_kd * kl + (1 - w_kd) * F.nll_loss(log_pred, target)
-
