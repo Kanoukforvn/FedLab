3c1104389557748ab33e14e0ff2aca6d31f0fede
Kanoukforvn <charly.fonb@gmail.com>
2024-05-21 11:06:30 +0700

    no scheduler lr = 0.0003

--- fedlab/contrib/algorithm/fednoro.py
+++ fedlab/contrib/algorithm/fednoro.py
@@ -105,9 +105,7 @@ class FedNoRoSerialClientTrainer(SGDSerialClientTrainer):
         self.batch_size = batch_size
         self.lr = lr
         self.optimizer = torch.optim.SGD(self._model.parameters(), lr)
-        self.scheduler = torch.optim.lr_scheduler.LambdaLR(
-            self.optimizer, lr_lambda=lambda epoch: 0.0003 / self.lr ** (epoch / epochs)
-        )
+
         self.criterion = torch.nn.CrossEntropyLoss()
         cls_num_list = self.get_num_of_each_class_global(self.dataset)
         for client_index, label_counts in enumerate(cls_num_list):
@@ -189,13 +187,8 @@ class FedNoRoSerialClientTrainer(SGDSerialClientTrainer):
 
                 optimizer.step()
                 epoch_loss.append(loss.item())
-            
-            # Log the learning rate
-            current_lr = optimizer.param_groups[0]['lr']
-            #logging.info(f'Epoch {epoch + 1}, Learning rate: {current_lr}')
-            
+                        
             avg_epoch_loss = sum(epoch_loss) / len(epoch_loss)
-            self.scheduler.step()  # Step the scheduler
 
         
         return [SerializationTool.serialize_model(self._model), avg_epoch_loss]
@@ -237,10 +230,5 @@ class FedNoRoSerialClientTrainer(SGDSerialClientTrainer):
 
             epoch_loss.append(np.array(batch_loss).mean())
             
-            # Log the learning rate
-            current_lr = self.optimizer.param_groups[0]['lr']
-            #logging.info(f'Epoch {epoch + 1}, Learning rate: {current_lr}')
-            
-            self.scheduler.step()  # Step the scheduler
 
         return SerializationTool.serialize_model(self._model), np.array(epoch_loss).mean()
