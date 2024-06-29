4d19949b5712f85e9e58d0cbda92f8692e1c3a77
Kanoukforvn <charly.fonb@gmail.com>
2024-06-29 13:34:28 +0700

    clean up

--- fednoro/test.py
+++
@@ -1,7 +0,0 @@
-import logging
-import time
-
-# Configure logging to both stdout and a log file
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.StreamHandler(), logging.FileHandler('log')])
-
-logging.info("test")
